d8320b03fb1eac971408d4884c1cf7da8ae2dd02
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2023-04-21 02:15:06 -0300

    criação do primeiro método, sem retorno e sem parametros, tbm trabalhamos a formatação na hora de imprimir os dados

--- Conta.cs
+++ Conta.cs
@@ -10,5 +10,12 @@ namespace Contas
         public string? Numero { get; set; }
         public DateTime DataAbertura { get; set; }
         public Decimal Saldo { get; set; }
+
+        public void ExibirExtrato()
+        {
+            System.Console.WriteLine($"Conta n°:  {Numero}");
+            System.Console.WriteLine($"Data de Abertura:  {DataAbertura.ToString("dd/MM/yyyy")}");
+            System.Console.WriteLine($"Saldo:  {Saldo.ToString("c")}");
+        }
     }
 }
