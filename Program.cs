1e5ea04b186ad8ab3ae3777b96b339cb97dbd38f
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2023-04-21 02:24:49 -0300

    testamos as chamadas aos metodos Depositar e Sacar

--- Program.cs
+++ Program.cs
@@ -4,4 +4,9 @@ var conta = new Conta();
 conta.Numero = "1";
 conta.DataAbertura = DateTime.Today;
 conta.Saldo = 100;
+
+conta.ExibirExtrato();
+conta.Depositar(50);
+conta.ExibirExtrato();
+conta.Sacar(100);
 conta.ExibirExtrato();
