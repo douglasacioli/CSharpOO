572de4b04010cf85c77873d246006c4ac4f7ef52
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2023-04-21 02:17:04 -0300

    chamando metodo

--- Program.cs
+++ Program.cs
@@ -3,7 +3,5 @@
 var conta = new Conta();
 conta.Numero = "1";
 conta.DataAbertura = DateTime.Today;
-conta.Saldo = 0;
-System.Console.WriteLine(conta.Numero);
-System.Console.WriteLine(conta.DataAbertura);
-System.Console.WriteLine(conta.Saldo);
+conta.Saldo = 100;
+conta.ExibirExtrato();
