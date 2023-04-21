534998dfeb0bf7441da5b87227df48438cd71dfe
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2023-04-21 02:24:06 -0300

    Criamos os metodos de Depositar e Sacar

--- Conta.cs
+++ Conta.cs
@@ -11,11 +11,22 @@ namespace Contas
         public DateTime DataAbertura { get; set; }
         public Decimal Saldo { get; set; }
 
-        public void ExibirExtrato()
+        public void ExibirExtrato() //metodo sem parametro
         {
             System.Console.WriteLine($"Conta n°:  {Numero}");
-            System.Console.WriteLine($"Data de Abertura:  {DataAbertura.ToString("dd/MM/yyyy")}");
-            System.Console.WriteLine($"Saldo:  {Saldo.ToString("c")}");
+            System.Console.WriteLine($"Data de Abertura:  {DataAbertura.ToString("dd/MM/yyyy")}"); //formata data
+            System.Console.WriteLine($"Saldo:  {Saldo.ToString("c")}"); //formata valor
+            System.Console.WriteLine("-----------------------------------------------------");
+        }
+
+        public void Depositar(decimal valor) //adiciona valor ao saldo
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor) //subtrai valor do saldo 
+        {
+            Saldo -= valor;
         }
     }
 }
